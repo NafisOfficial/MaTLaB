a21424772fa9c1321c35de69d51449ef35ed0b85
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-09-07 22:55:30 +0600

    update

--- False_position.m
+++ False_position.m
@@ -5,18 +5,18 @@ f=eval(['@(x)',fx]);
 a=input('Enter a=');
 b=input('Enter b=');
 s=1;
-fprintf("N\t\t   a\t\t   b\t\t   x\t\t   f(x)\t\t   Error\n");
+fprintf("N\t\t a\t\t b\t\t x\t\t f(x)\t\t Error\n");
 for k=1:100;
     x(k) = a-(f(a)*(b-a))/(f(b)-f(a));
     c=f(x(k));
-    printf("%g\t %f\t %f\t %f\t %f\t %f\n",k,a,b,x(k),c,s);
+    fprintf("%g\t %f\t %f\t %f\t %f\t %f\n",k,a,b,x(k),c,s);
     if(c>0)
         b=x(k);
     else
         a=x(k);
     end
-    x(k+1) = a - (f(a)*(b-a))/(f(b)-f(a));
-    s = ((abs(x(k+a)-x(k)))/abs(x(k+1)))*100;
+    x(k+1)=a-(f(a)*(b-a))/(f(b)-f(a));
+    s=((abs(x(k+1)-x(k)))/abs(x(k+1)))*100;
     if s<=.01
         break
     end
